01427442cc467b68c18eaf580943512ed9595983
regex01-git <fardeenkhan4807@gmail.com>
2025-08-03 11:14:00 -0700

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -12,7 +12,7 @@ require("dotenv").config()  // BECAUSE OF THIS YOU CONNECT THIS FILE TO .env FIL
 const app=express();
 app.use(express.json({limit:'50mb'}));
 app.use(cors({
-  origin:'http://localhost:5173',
+  origin:'https://raos-industry.onrender.com',
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   // allowedHeaders: ['Content-Type', 'Authorization']
   allowedHeaders: ["Content-Type", "x-auth-token"] 
