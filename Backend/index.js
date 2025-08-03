cc872599a4c37701691be35fdcb1ffc4e1ffcc9d
regex01-git <fardeenkhan4807@gmail.com>
2025-08-03 13:29:31 -0700

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -12,7 +12,7 @@ const port=process.env.PORT||5000
 const app=express();
 app.use(express.json({limit:'50mb'}));
 app.use(cors({
-  origin:'https://raos-industry.onrender.com',
+  origin:'https://raos-industry.onrender.com/',
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   // allowedHeaders: ['Content-Type', 'Authorization']
   allowedHeaders: ["Content-Type", "x-auth-token"] 
