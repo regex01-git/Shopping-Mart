927e01db330e8509545ed33c207855efdbec1de6
regex01-git <fardeenkhan4807@gmail.com>
2025-08-03 13:51:45 -0700

    Update auth.js

--- Backend/middleware/auth.js
+++ Backend/middleware/auth.js
@@ -1,6 +1,6 @@
 const jwt=require("jsonwebtoken");
 const auth=(req,res,next)=>{
-    const token=req.header('x-auth-token');
+    const token=req.header("x-auth-token");
     if(!token) return res.status(401).send('Access denied first need to log in');
     try{
         const decoded=jwt.verify(token,process.env.JWT_SECRET_KEY);
